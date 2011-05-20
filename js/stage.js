3a31f924a7fcc311dff6af8a33d2571127f3093c
Dan Gritsko <dgritsko@gmail.com>
2011-05-20 17:46:38 -0400

    added partial implementation of generateStages function

--- js/stage.js
+++ js/stage.js
@@ -20,7 +20,33 @@ Mduel.Stage.stage = function(spec) {
    return that;
 }
 
-Mduel.Stage.generateStage = function() {
+Mduel.Stage.generateStage = function(width) {
+   var maxPlatformWidth = 7;
+   var minPlatformWidth = 2;
+   var maxGapWidth = 2;
+   var minGapWidth = 1;
+   
+   // Min 2, Max 4
+   var numPlatforms = Math.floor(Math.random() * 3) + 2;
+   
+   var level = new Array();
+   
+   for (var i = 0; i < numPlatforms; i++)
+   {
+      // Gap
+      var gapWidth = Math.floor(Math.random() * (maxGapWidth + 1 - minGapWidth)) + minGapWidth;
+      level.push({ width: gapWidth, isPlatform: false });
+
+      // Level
+      var platformWidth = Math.floor(Math.random() * (maxPlatformWidth + 1 - minPlatformWidth)) + minPlatformWidth;
+      level.push({ width: platformWidth, isPlatform: true });
+   }
+   
+   var lastGapWidth = Math.floor(Math.random() * (maxGapWidth + 1 - minGapWidth)) + minGapWidth;
+   level.push({ width: lastGapWidth, isPlatform: false });
+   
+   
+   // TODO: Finish conversion of algorithm to generate platforms
    
    var toggle = Math.floor(Math.random()*2);
 
