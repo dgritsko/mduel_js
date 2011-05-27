049abc3c3655f87102cb7067fcfdade4b65c0f39
Dan Gritsko <dgritsko@gmail.com>
2011-05-27 07:28:48 -0400

    added initial key mapping mechanism to keyboard.js

--- js/keyboard.js
+++ js/keyboard.js
@@ -8,6 +8,20 @@ if (typeof Mduel.Keyboard == 'undefined') {
 Mduel.Keyboard.keyDown = function(e) {
    var keyName = Mduel.Keyboard.getKeyName(e.keyCode);
    if (keyName) {
+      for (var i = 0, len = Mduel.Keyboard.playerKeys.length; i < len; i++) {
+         if (Mduel.Keyboard.playerKeys[i][keyName]) {
+            var mapping = Mduel.Keyboard.playerKeys[i][keyName];
+            
+            var eventTime = new Date().valueOf();
+        
+            Mduel.Keyboard.playerKeyStates[i][mapping].pressed = true;
+            Mduel.Keyboard.playerKeyStates[i][mapping].eventTime = eventTime;
+            Mduel.Keyboard.playerKeyStates[i].lastKey.name = mapping;
+            Mduel.Keyboard.playerKeyStates[i].lastKey.pressed = true;
+            Mduel.Keyboard.playerKeyStates[i].lastKey.eventTime = eventTime;
+         }
+      }
+   
       for (var i = 0, len = Mduel.Game.players.length; i < len; i++) {
          if (Mduel.Game.players[i].keyDown) {
             Mduel.Game.players[i].keyDown(keyName);
@@ -19,6 +33,21 @@ Mduel.Keyboard.keyDown = function(e) {
 Mduel.Keyboard.keyUp = function(e) {
    var keyName = Mduel.Keyboard.getKeyName(e.keyCode);
    if (keyName) {
+      for (var i = 0, len = Mduel.Keyboard.playerKeys.length; i < len; i++) {
+         if (Mduel.Keyboard.playerKeys[i][keyName]) {
+            var mapping = Mduel.Keyboard.playerKeys[i][keyName];
+            
+            var eventTime = new Date().valueOf();
+         
+            Mduel.Keyboard.playerKeyStates[i][mapping].pressed = false;
+            Mduel.Keyboard.playerKeyStates[i][mapping].eventTime = eventTime;
+            Mduel.Keyboard.playerKeyStates[i].lastKey.name = mapping;
+            Mduel.Keyboard.playerKeyStates[i].lastKey.pressed = false;
+            Mduel.Keyboard.playerKeyStates[i].lastKey.eventTime = eventTime;
+         }
+      }
+   
+      // TODO
       for (var i = 0, len = Mduel.Game.players.length; i < len; i++) {
          if (Mduel.Game.players[i].keyUp) {
             Mduel.Game.players[i].keyUp(keyName);
@@ -26,6 +55,42 @@ Mduel.Keyboard.keyUp = function(e) {
       }
    }
 }
+
+Mduel.Keyboard.playerKeys = [
+   {
+      'left' : 'left',
+      'right' : 'right',
+      'up' : 'up', 
+      'down' : 'down',
+      'enter' : 'action'
+   },
+   {
+      'a' : 'left',
+      'd' : 'right',
+      'w' : 'up', 
+      's' : 'down',
+      'q' : 'action'   
+   }
+];
+
+Mduel.Keyboard.playerKeyStates = [
+   {
+      'lastKey' : { name: undefined, pressed: false, eventTime: 0 },
+      'up' : { pressed : false, eventTime : 0 },
+      'down' : { pressed : false, eventTime : 0 },
+      'left' : { pressed : false, eventTime : 0 },
+      'right' : { pressed : false, eventTime : 0 },
+      'action' : { pressed : false, eventTime : 0 }
+   },
+   {
+      'lastKey' : { name: undefined, pressed: false, eventTime: 0 },
+      'up' : { pressed : false, eventTime : 0 },
+      'down' : { pressed : false, eventTime : 0 },
+      'left' : { pressed : false, eventTime : 0 },
+      'right' : { pressed : false, eventTime : 0 },
+      'action' : { pressed : false, eventTime : 0 }
+   }   
+];
 
 Mduel.Keyboard.keyNames = {
       '37' : 'left',
@@ -41,7 +106,8 @@ Mduel.Keyboard.keyNames = {
       '74' : 'j',
       '75' : 'k',
       '76' : 'l',
-      '85' : 'u'      
+      '85' : 'u',
+      '81' : 'q'
 };
 
 Mduel.Keyboard.getKeyName = function(keyCode) {
