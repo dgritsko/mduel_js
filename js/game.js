37ab7d88d971bf11b0129f37f3cd4e69db6912e1
Dan Gritsko <dgritsko@gmail.com>
2018-01-10 22:39:55 -0500

    Working on level generation and such

--- js/game.js
+++ js/game.js
@@ -54,25 +54,32 @@
         const spawnWidth = 4;
 
         const platforms = game.add.group();
+        const platformInfos = generatePlatforms();
 
-        for (let i = 0; i < (levelHeight - 1); i++) {
-            for (let j = 0; j < levelWidth; j++) {
-                const platform = game.add.sprite((j * horizontalSpacing) + horizontalOffset, (i * verticalSpacing) + verticalOffset, 'main_platform');
-                platforms.add(platform);
-            }
-        }
-
-        // Left spawn
-        for (let j = 0; j < spawnWidth; j++) {
-            const platform = game.add.sprite((j * horizontalSpacing) + horizontalOffset * 1.5, (4 * verticalSpacing) + verticalOffset, 'spawn_platform');
+        platformInfos.forEach(p => {
+            const spriteName = p.isSpawn ? 'spawn_platform' : 'main_platform';
+            const platform = game.add.sprite(p.x, p.y, spriteName);
             platforms.add(platform);
-        }
+        });
 
-        // Right spawn
-        for (let j = levelWidth - spawnWidth + 1; j < levelWidth + 1; j++) {
-            const platform = game.add.sprite((j * horizontalSpacing) - horizontalOffset * 0.5, (4 * verticalSpacing) + verticalOffset, 'spawn_platform');
-            platforms.add(platform);
-        }
+        // for (let i = 0; i < (levelHeight - 1); i++) {
+        //     for (let j = 0; j < levelWidth; j++) {
+        //         const platform = game.add.sprite((j * horizontalSpacing) + horizontalOffset, (i * verticalSpacing) + verticalOffset, 'main_platform');
+        //         platforms.add(platform);
+        //     }
+        // }
+
+        // // Left spawn
+        // for (let j = 0; j < spawnWidth; j++) {
+        //     const platform = game.add.sprite((j * horizontalSpacing) + horizontalOffset * 1.5, (4 * verticalSpacing) + verticalOffset, 'spawn_platform');
+        //     platforms.add(platform);
+        // }
+
+        // // Right spawn
+        // for (let j = levelWidth - spawnWidth + 1; j < levelWidth + 1; j++) {
+        //     const platform = game.add.sprite((j * horizontalSpacing) - horizontalOffset * 0.5, (4 * verticalSpacing) + verticalOffset, 'spawn_platform');
+        //     platforms.add(platform);
+        // }
 
         platforms.children.forEach(p => {
             game.physics.enable(p);
@@ -82,12 +89,12 @@
 
         // Ropes
         // TODO
-        for (let i = 0; i < levelHeight; i++) {
-            for (let j = 0; j < levelWidth; j++) {
-                const anchor = game.add.sprite((j * horizontalSpacing) + 47, (i * verticalSpacing) + 32, 'rope_anchor');
-                anchor.anchor.setTo(0.5);
-            }
-        }
+        const ropes = generateRopes();
+
+        ropes.forEach(r => {
+            const anchor = game.add.sprite((r.x * horizontalSpacing) + 47, (r.y * verticalSpacing) + 32, 'rope_anchor');
+            anchor.anchor.setTo(0.5);
+        });
 
         // Powerup spawns
         const topSpawn = game.add.sprite(game.world.centerX, 0, 'powerup_spawn');
@@ -117,6 +124,103 @@
             rightSpawn,
             marshmallows
         }
+    }
+
+    const generatePlatforms = () => {
+        const result = [];
+
+        const verticalSpacing = 64;
+        const verticalOffset = 80;
+
+        const horizontalSpacing = 32;
+        const horizontalOffset = 32;
+
+        const levelHeight = 5;
+        const levelWidth = 18;
+
+        const spawnWidth = 4;
+
+        // Left spawn
+        for (let j = 0; j < spawnWidth; j++) {
+            result.push({ x: (j * horizontalSpacing) + horizontalOffset * 1.5, y: (4 * verticalSpacing) + verticalOffset, isSpawn: true });
+        }
+
+        // Right spawn
+        for (let j = levelWidth - spawnWidth + 1; j < levelWidth + 1; j++) {
+            result.push({ x: (j * horizontalSpacing) - horizontalOffset * 0.5, y: (4 * verticalSpacing) + verticalOffset, isSpawn: true });
+        }
+
+        // Top left
+        for (let j = 2; j < 6; j++) {
+            result.push({ x: (j * horizontalSpacing) + horizontalOffset, y: verticalOffset, isSpawn: false });
+        }
+        
+        // Top right
+        for (let j = 12; j < 16; j++) {
+            result.push({ x: (j * horizontalSpacing) + horizontalOffset, y: verticalOffset, isSpawn: false });
+        }
+
+        // Randomized platforms
+        for (let i = 1; i < 4; i++) {
+            const maxPlatformWidth = 7;
+            const minPlatformWidth = 2;
+            const maxGapWidth = 2;
+            const minGapWidth = 1;
+
+            let isPlatform = Math.floor(Math.random() * 2);
+            const parts = [];
+
+            const currentWidth = () => (_.reduce(_.pluck(parts, 'width'), (a, b) => (a + b), 0));
+
+            while (currentWidth() < levelWidth) {
+                const minWidth = isPlatform ? minPlatformWidth : minGapWidth;
+                const maxWidth = isPlatform ? maxPlatformWidth : maxGapWidth;
+    
+                const newWidth = Math.floor(Math.random() * (maxWidth + 1 - minWidth)) + minWidth;
+    
+                parts.push({ width: newWidth, isPlatform: isPlatform });
+
+                isPlatform = !isPlatform;
+
+                // const currentLevelWidth = currentWidth();
+                // if (currentLevelWidth > levelWidth) {
+                //     const oldWidth = parts[parts.length - 1].width;
+
+                //     parts[parts.length - 1].width -= (currentLevelWidth - levelWidth);
+                //     parts[parts.length - 1].isPlatform = false;
+
+                //     const newWidth = parts[parts.length - 1].width;
+                // }
+            }
+
+            let counter = 0;
+            for (let j = 0; j < parts.length; j++) {
+                
+                for (let k = 0; k < parts[j].width; k++) {
+                    const x = j + k + counter;
+
+                    if (parts[j].isPlatform && x < levelWidth) {
+                        result.push({ x: (x * horizontalSpacing) + horizontalOffset, y: (i * verticalSpacing) + verticalOffset, isSpawn: false });
+                    }
+                }
+
+                counter += parts[j].width;
+            }
+        }
+
+        return result;
+    }
+
+    const generateRopes = () => {
+        const result = [];
+
+        // Fixed top ropes
+        result.push({ x: 3.5, y: 0, length: 5 });
+        result.push({ x: 13.5, y: 0, length: 5 });
+
+        // TODO: Other ropes
+
+        return result;
     }
 
     MarshmallowDuel.Game = {preload: preload, create: create, update: update};
