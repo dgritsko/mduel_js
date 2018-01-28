31c3936e7e3faa58e0451ef009aa91951122701c
Dan Gritsko <dgritsko@gmail.com>
2018-01-28 00:26:34 -0500

    Moving level generation code out of game.js and into helper files

---
+++ src/gameutil/level.js
@@ -0,0 +1,81 @@
+import cfg from '../gameConfig';
+import { generatePlatforms, generateRopes } from '../util/level';
+
+function makeLevel() {
+    const { verticalSpacing, verticalOffset, horizontalSpacing, horizontalOffset } = cfg;
+
+    const levelHeight = 5;
+    const levelWidth = 18;
+
+    const spawnWidth = 4;
+
+    const platforms = game.add.group();
+    const platformInfos = generatePlatforms();
+
+    platformInfos.forEach(info => {
+        const x = info.column * horizontalSpacing + horizontalOffset;
+        const y = info.row * verticalSpacing + verticalOffset;
+        const spriteName = info.isSpawn ? 'spawn_platform' : 'main_platform';
+        const platform = game.add.sprite(x, y, spriteName);
+        platforms.add(platform);
+    });
+
+    platforms.children.forEach(p => {
+        game.physics.enable(p);
+        p.body.moves = false;
+        p.body.immovable = true;
+    });
+
+    // Ropes
+    const ropes = generateRopes(platformInfos);
+
+    ropes.forEach(r => {
+        r.x = (r.column * horizontalSpacing) + 47;
+        r.y = (r.row * verticalSpacing) + 32;
+        r.height = verticalSpacing * r.length - 15
+
+        const graphics = game.add.graphics(r.x + 0.5, r.y);
+        graphics.lineStyle(2, 0x8C6414, 1);
+        graphics.lineTo(0, r.height);
+
+        const anchor = game.add.sprite(r.x, r.y, 'rope_anchor');
+        anchor.anchor.setTo(0.5);
+    });
+
+    // Powerup spawns
+    const topSpawn = game.add.sprite(game.world.centerX, 0, 'powerup_spawn');
+    topSpawn.anchor.setTo(0.5, 0);
+
+    const powerupYOffset = -32;
+    const leftSpawn = game.add.sprite(0, game.world.height / 2 + powerupYOffset, 'powerup_spawn');
+    leftSpawn.anchor.setTo(0.5, 0);
+    leftSpawn.angle = -90;
+
+    const rightSpawn = game.add.sprite(game.world.width, game.world.height / 2 + powerupYOffset, 'powerup_spawn');
+    rightSpawn.anchor.setTo(0.5, 0);
+    rightSpawn.scale.setTo(-1, 1);
+    rightSpawn.angle = 90;
+
+    // Marshmallows
+    const marshmallows = game.add.group();
+    for (let j = 0; j < (game.world.width / horizontalSpacing); j++) {
+        const mallowSurface = game.add.sprite(j * horizontalSpacing, game.world.height - (horizontalSpacing * 1.75), 'mallow_surface');
+        mallowSurface.animations.add('default', [j % 4, (j + 1) % 4, (j + 2) % 4, (j + 3) % 4], 0.25, true);
+        mallowSurface.animations.play('default');
+        marshmallows.add(mallowSurface);
+
+        const marshmallow = game.add.sprite(j * horizontalSpacing, game.world.height - horizontalSpacing, 'mallow');
+        marshmallows.add(marshmallow);
+    }
+
+    return {
+        platforms,
+        topSpawn,
+        leftSpawn,
+        rightSpawn,
+        marshmallows,
+        ropes
+    }
+}
+
+export { makeLevel };
---
+++ src/util/level.js
@@ -0,0 +1,129 @@
+import cfg from '../gameConfig';
+
+function generateRopes(platforms) {
+    const result = [];
+
+    // Fixed top ropes
+    result.push({ column: 3.5, row: 0, length: 5 });
+    result.push({ column: 13.5, row: 0, length: 5 });
+
+    const leftRopes = [];
+    const otherRopes = [];
+
+    const isPlatform = (column, row) => (platforms.filter(p => p.row == row && p.column == column).length > 0)
+
+    for (let i = 1; i < 18; i++) {
+        const options = [];
+        if (isPlatform(i, 1) && isPlatform(i, 2)) {
+            options.push({ column: i, row: 1, length: 2 });
+        }
+        if (isPlatform(i, 2) && isPlatform(i, 3)) {
+            options.push({ column: i, row: 2, length: 2 });
+        }
+        if (isPlatform(i, 1) && isPlatform(i, 3)) {
+            options.push({ column: i, row: 1, length: 3 });
+        }
+
+        if (options.length > 0) {
+            const item = Phaser.ArrayUtils.getRandomItem(options);
+
+            if (i < 4) {
+                leftRopes.push(item)
+            } else if (i > 4 && i != 14) {
+                otherRopes.push(item);
+            }
+        }
+    }
+
+    if (leftRopes.length > 0) {
+        result.push(Phaser.ArrayUtils.getRandomItem(leftRopes));
+    }
+
+    if (otherRopes.length > 0) {
+        // Hard max of other ropes is 4
+        const max = Math.min(otherRopes.length, Math.floor(Math.random() * 3) + 1);
+
+        let count = 0;
+        while (count < max && otherRopes.length > 0) {
+            result.push(Phaser.ArrayUtils.removeRandomItem(otherRopes));
+        
+            count++;
+        }   
+    }
+
+    return result;
+}
+
+function generatePlatforms() {
+    let result = [];
+
+    const { verticalSpacing, verticalOffset, horizontalSpacing, horizontalOffset } = cfg;
+
+    const levelHeight = 5;
+    const levelWidth = 18;
+
+    const spawnWidth = 4;
+
+    // Left spawn
+    for (let j = 0; j < spawnWidth; j++) {
+        result.push({ row: 4, column: j + 0.5, isSpawn: true });
+    }
+
+    // Right spawn
+    for (let j = levelWidth - spawnWidth + 1; j < levelWidth + 1; j++) {
+        result.push({ row: 4, column: j - 1.5, isSpawn: true });
+    }
+
+    // Top left
+    for (let j = 2; j < 6; j++) {
+        result.push({ row: 0, column: j, isSpawn: false });
+    }
+    
+    // Top right
+    for (let j = 12; j < 16; j++) {
+        result.push({ row: 0, column: j, isSpawn: false });
+    }
+
+    // Randomized platforms
+    for (let i = 1; i < 4; i++) {
+        const maxPlatformWidth = 7;
+        const minPlatformWidth = 2;
+        const maxGapWidth = 2;
+        const minGapWidth = 1;
+
+        let curr = [];
+        while (curr.length < levelWidth) {
+            // 50% of the time, choose to start the level with a gap
+            if (curr.length == 0 && game.rnd.integerInRange(0, 1) == 1) {
+                for (var j = 0; j < game.rnd.integerInRange(0, maxGapWidth); j++) {
+                    curr.push(false);
+                }
+            } else {
+                for (let j = 0; j < game.rnd.integerInRange(minPlatformWidth, maxPlatformWidth); j++) {
+                    curr.push(true);
+                }
+                for (let j = 0; j < game.rnd.integerInRange(minGapWidth, maxGapWidth); j++) {
+                    curr.push(false);
+                }
+            }
+        }
+        curr = curr.slice(0, levelWidth);
+        
+        // Since we can run past the max level width, slicing off the extra can result in a 
+        // 1-width platform at the end of the level. In this case, make sure it's at least 
+        // length 2
+        if (curr[levelWidth - 1] && !curr[levelWidth - 2]) {
+            curr[levelWidth - 2] = true;
+        }
+
+        const temp = curr.map((value, index) => ({ value, index }))
+            .filter(item => item.value)
+            .map(item => ({ row: i, column: item.index, isSpawn: false }));
+        
+        result = result.concat(temp);
+    }
+
+    return result;
+}
+
+export { generateRopes, generatePlatforms };
