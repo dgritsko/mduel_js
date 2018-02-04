d323324679fd85720ae07c4699e56b80f686d44c
Dan Gritsko <dgritsko@gmail.com>
2018-02-04 01:49:45 -0500

    Adding some fonts

--- src/states/boot/index.js
+++ src/states/boot/index.js
@@ -1,19 +1,30 @@
 function preload() {
-    game.load.spritesheet('player1', 'assets/player1.png', 64, 64);
-    game.load.spritesheet('player2', 'assets/player2.png', 64, 64);
-    game.load.image('main_platform', 'assets/main_platform.png');
-    game.load.image('spawn_platform', 'assets/spawn_platform.png');   
-    game.load.image('powerup_spawn', 'assets/powerup_spawn.png');   
-    game.load.image('rope_anchor', 'assets/rope_anchor.png');   
-    game.load.image('mallow', 'assets/mallow.png');   
-    game.load.spritesheet('mallow_surface', 'assets/mallow_surface.png', 32, 32);   
+    game.load.spritesheet("player1", "assets/player1.png", 64, 64);
+    game.load.spritesheet("player2", "assets/player2.png", 64, 64);
+    game.load.image("main_platform", "assets/main_platform.png");
+    game.load.image("spawn_platform", "assets/spawn_platform.png");
+    game.load.image("powerup_spawn", "assets/powerup_spawn.png");
+    game.load.image("rope_anchor", "assets/rope_anchor.png");
+    game.load.image("mallow", "assets/mallow.png");
+    game.load.spritesheet(
+        "mallow_surface",
+        "assets/mallow_surface.png",
+        32,
+        32
+    );
+
+    game.load.bitmapFont(
+        "mduel",
+        "assets/fonts/pc-senior.png",
+        "assets/fonts/pc-senior.fnt"
+    );
 }
 
 function create() {
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
 
-    game.state.start('Game');
+    game.state.start("Game");
 }
 
-export default {preload: preload, create: create};
+export default { preload: preload, create: create };
