b6a6ad9dd2096ae5bf01deb09b2ec56dedf9feea
Dan Gritsko <dgritsko@gmail.com>
2018-02-08 00:16:53 -0500

    Add update method to PlayerSnapshot

--- src/playerSnapshot.js
+++ src/playerSnapshot.js
@@ -3,6 +3,15 @@ class PlayerSnapshot {
         this.player = player;
         this.state = Object.assign({}, player.getState(), player.getInput());
     }
+
+    update(toUpdate) {
+        this.player.update(toUpdate);
+        this.state = Object.assign(
+            {},
+            this.player.getState(),
+            this.player.getInput()
+        );
+    }
 }
 
 export { PlayerSnapshot };
